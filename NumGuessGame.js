9f9c85800429f9537afb9432403a86d4cf2d272d
raedhashmi <163880042+raedhashmi@users.noreply.github.com>
2024-06-30 17:52:39 +0500

    Add files via upload

---
+++ NumGuessGame.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+export default async () => {
+    function getRandom() {
+        let randomNum = Math.floor(Math.random() * 10);
+        inquirer.prompt([{
+                name: "userguess",
+                message: "What is your guess?",
+                type: "input"
+            }])
+            .then((answers) => {
+            if (answers.userguess == "") {
+                console.log(chalk.red("ERR! Cannot be blank"));
+            }
+            if (answers.userguess == randomNum) {
+                console.log("Correct");
+                getRandom();
+            }
+            else if (answers.userguess != randomNum)
+                (console.log("Incorrect. Try again\nAnswer was", randomNum)),
+                    getRandom();
+        });
+    }
+    getRandom();
+};
